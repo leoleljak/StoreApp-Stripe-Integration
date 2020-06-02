0f5f9197a7e202ae2d6b7bafa1a183ccf0ef3bf7
Leo Leljak <leo.leljak@gmail.com>
2020-06-02 18:27:47 +0200

    #4 CollectionViewFlowLayout

---
+++ Client/StoreApp-Stripe-Integration/Common/Utilities/UIHelper.swift
@@ -0,0 +1,28 @@
+//
+//  UIHelper.swift
+//  StoreApp-Stripe-Integration
+//
+//  Created by Leo Leljak on 02/06/2020.
+//  Copyright © 2020 Leo Leljak. All rights reserved.
+//
+
+import UIKit
+
+enum UIHelper {
+    
+    static func createThreeColumnFlowLayout(for view:UIView) -> UICollectionViewFlowLayout {
+        let width = view.bounds.width
+        let padding: CGFloat = 12
+        let minimumItemSpacing: CGFloat = 10
+        let availableWidth = width -  (padding * 2) - (minimumItemSpacing * 2)
+        let itemWidth = availableWidth / 2
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
+        
+        
+        return flowLayout
+    }
+    
+}

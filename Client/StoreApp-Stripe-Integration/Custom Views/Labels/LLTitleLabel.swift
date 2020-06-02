4c34760c9eaa1698393457d49ed9ef0cc332be8e
Leo Leljak <leo.leljak@gmail.com>
2020-06-02 18:27:10 +0200

    #4 Created custom titleLabel

---
+++ Client/StoreApp-Stripe-Integration/Custom Views/Labels/LLTitleLabel.swift
@@ -0,0 +1,44 @@
+//
+//  LLTitleLabel.swift
+//  StoreApp-Stripe-Integration
+//
+//  Created by Leo Leljak on 02/06/2020.
+//  Copyright © 2020 Leo Leljak. All rights reserved.
+//
+
+import UIKit
+
+class LLTitleLabel: UILabel {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    init(fontSize: CGFloat) {
+        super.init(frame: .zero)
+        font = UIFont.systemFont(ofSize: fontSize, weight: .medium)
+        configure()
+    }
+    
+    
+    convenience init(fontSize: CGFloat, fontColor: UIColor) {
+        self.init(fontSize: fontSize)
+        textColor = fontColor
+    }
+    
+    
+    private func configure(){
+        
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.90
+        lineBreakMode = .byTruncatingTail
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}

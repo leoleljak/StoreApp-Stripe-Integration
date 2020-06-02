8aec4ac65af2a094ccb8cef513754756349541d7
Leo Leljak <leo.leljak@gmail.com>
2020-06-02 18:27:28 +0200

    #4 Created Custom ProductImage

---
+++ Client/StoreApp-Stripe-Integration/Custom Views/UIImageView/ProductImage.swift
@@ -0,0 +1,58 @@
+//
+//  ProductImage.swift
+//  StoreApp-Stripe-Integration
+//
+//  Created by Leo Leljak on 02/06/2020.
+//  Copyright © 2020 Leo Leljak. All rights reserved.
+//
+
+import UIKit
+
+class ProductImage: UIImageView {
+
+    let activityIndicator = UIActivityIndicatorView(style: .large)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    func configure() {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.clipsToBounds = true
+        self.contentMode = .scaleAspectFill
+        layer.cornerRadius = 15
+    }
+    
+    
+    func configureActivityIndicator() {
+        addSubview(activityIndicator)
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        NSLayoutConstraint.activate([
+            activityIndicator.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            activityIndicator.centerXAnchor.constraint(equalTo: self.centerXAnchor)
+        ])
+        activityIndicator.startAnimating()
+    }
+    
+    
+    func stopAnimatingActivity() {
+        activityIndicator.stopAnimating()
+    }
+    
+    func startAnimatingActivity() {
+           activityIndicator.startAnimating()
+    }
+    
+    
+    
+    
+}
